68bead7933b3e54782b511be0015bdae9c443a9e
moses <mu.mobile.mu@googlemail.com>
2026-02-11 21:58:38 +0000

    updates

--- fish/config.fish
+++ fish/config.fish
@@ -4,8 +4,8 @@ set -g fish_greeting
 
 starship init fish | source
 
-alias ls "exa -l --group-directories-first"
 alias hx helix
+alias ls "lsd -l --group-dirs first"
 
 if status is-interactive
     # Commands to run in interactive sessions can go here
