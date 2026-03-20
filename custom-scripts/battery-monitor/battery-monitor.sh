c2dbe8f9b9dd58e79063fd931686817bdcb22acb
moses <mu.mobile.mu@googlemail.com>
2026-03-20 22:51:34 +0000

    updates

--- custom-scripts/battery-monitor/battery-monitor.sh
+++ custom-scripts/battery-monitor/battery-monitor.sh
@@ -14,7 +14,7 @@ while true; do
             notify-send -u critical -i battery-caution "BATTERY CRITICAL" "Level: ${PERCENT}% - Plug in NOW!"
             SLEEP_TIME=120  # Check every 120 seconds
         elif [ "$PERCENT" -le 20 ]; then
-            notify-send -u normal -i battery-low "Battery Low" "Level: ${PERCENT}%"
+            notify-send -u normal -i battery-low -t 10000 "Battery Low" "Level: ${PERCENT}%"
             SLEEP_TIME=300  # Check every 5 minutes
         elif [ "$PERCENT" -le 30 ]; then
             SLEEP_TIME=600 # Check every 10 minutes
