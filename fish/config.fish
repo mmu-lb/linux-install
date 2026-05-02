127a06e765bc2bc5f80958e26c06a1028baac56d
moses <mu.mobile.mu@googlemail.com>
2026-05-02 20:02:42 +0100

    updates

--- fish/config.fish
+++ fish/config.fish
@@ -7,6 +7,16 @@ starship init fish | source
 alias hx helix
 alias ls "lsd -l --group-dirs first"
 
+function zen-open
+    # Check if a filename was provided
+    if count $argv >/dev/null
+        # Open the file, silence output, background it, and disown
+        zen-browser $argv >/dev/null 2>&1 & disown
+    else
+        zen-browser >/dev/null 2>&1 & disown
+    end
+end
+
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
